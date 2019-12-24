dad337dd7eebc43795ac03ad6a8bddd1d75cdf30
Sandeep Mishra <sunnyid1972@gmail.com>
2019-12-24 23:25:13 +0530

    first

---
+++ src/KMP.java
@@ -0,0 +1,88 @@
+
+public class KMP {
+
+	public static void main(String[] args) {
+		String txt="ABCABAAAABAB";
+		String pat="ABC";
+		
+		KMP kmp=new KMP();
+		kmp.kmpSearch(txt, pat);
+
+	}
+	
+	public void kmpSearch(String txt,String pat)
+	{
+		int LPS[]=new int[pat.length()];
+		
+		
+		LPS=constructLPS(LPS,txt,pat);
+		
+		int patSize=pat.length();
+		int txtSize=txt.length();
+		int i=0;
+		int j=0;
+		while(i<txtSize)
+		{
+			if(pat.charAt(j)==txt.charAt(i))
+			{
+				i++;
+				j++;
+				if(patSize==j)
+				{
+					System.out.println("match found at index"+ (i-j));
+					j=LPS[j-1];
+				}
+			}
+			else if(pat.charAt(j)!=txt.charAt(i))
+			{
+				if(j!=0)
+				{
+					j=LPS[j-1];
+				}
+				else{
+					i++;
+					
+				}
+			}
+		}
+		
+		
+		
+	}
+	
+	
+	public int[] constructLPS(int[] LPS,String txt,String pat)
+	{
+		int i=1;
+		int j=0;
+		LPS[0]=0;
+		
+		while(i < LPS.length)
+		{
+			if(pat.charAt(i) == pat.charAt(j))
+			{
+				j++;
+				LPS[i] = j;
+				i++;
+			}
+			else{
+				if(j != 0)
+				{
+					j = LPS[j-1];
+				}
+				else{
+					
+					LPS[i] = j;
+					i++;
+				}
+			}
+		}
+		
+		for(int y=0;y<LPS.length;y++)
+		{
+			System.out.print(LPS[y]);
+		}
+		return LPS;
+	}
+
+}
